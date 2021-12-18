20dd807acbe6f1582516d2c1e7d9419fbd274086
Yogesh Narendra Tiwari <92584567+yogeshnarendratiwari@users.noreply.github.com>
2021-12-18 12:55:23 +0530

    18-12-2021

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,37 +13,128 @@ void main() {
           )),
           backgroundColor: Colors.black,
         ),
-        body: DiceePage(),
+        body: DicePage(),
       ),
     ),
   );
 }
 
-class DiceePage extends StatelessWidget {
-  const DiceePage({Key? key}) : super(key: key);
+
+class DicePage extends StatefulWidget {
+  const DicePage({Key? key}) : super(key: key);
 
   @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDice = 1;
+  int rightDice = 1;
+  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Row(
-        children: [
-          Expanded(
-            child: TextButton(
-              onPressed: (){
-                print('Left Button pressed');
-              },
-                child: Image.asset('images/dice1.png')),
-          ),
-          Expanded(
-            child: TextButton(
-              onPressed: (){
-                print('Right Button pressed');
-              },
-              child: Image.asset('images/dice1.png'),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Row(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Image.asset('images/dice$leftDice.png'),
+              ),
             ),
-          ),
-        ],
-      ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Image.asset('images/dice$rightDice.png'),
+              ),
+            ),
+          ],
+        ),
+
+     const  SizedBox(
+         height : 100.0,
+       ),
+       Row(
+         mainAxisAlignment: MainAxisAlignment.center,
+         children: [
+           SizedBox(
+             width: 150.0,
+             height: 50.0,
+             child: ElevatedButton(onPressed: (){
+                 setState(() {
+                   leftDice = Random().nextInt(6)+1;
+                   rightDice = Random().nextInt(6)+1;
+                 });
+             }, child: const Text('Roll Both Dices'),
+
+               style: ButtonStyle
+                 (
+                 backgroundColor: MaterialStateProperty.all(Colors.black),
+                 shadowColor: MaterialStateProperty.all(Colors.white),
+                 foregroundColor: MaterialStateProperty.all(Colors.white),
+
+               ),
+
+
+             ),
+           )
+         ],
+       ),
+        SizedBox(
+          height: 50.0,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: 170.0,
+              height: 50.0,
+              child: ElevatedButton(onPressed: (){
+                setState(() {
+                  leftDice = Random().nextInt(6)+1;
+                });
+              }, child: const Text('Roll Only Left Dice'),
+
+                style: ButtonStyle
+                  (
+                  backgroundColor: MaterialStateProperty.all(Colors.black),
+                  shadowColor: MaterialStateProperty.all(Colors.white),
+                  foregroundColor: MaterialStateProperty.all(Colors.white),
+
+                ),
+
+
+              ),
+            ),
+
+            SizedBox(
+              width: 10.0,
+            ),
+            SizedBox(
+              width: 170.0,
+              height: 50.0,
+              child: ElevatedButton(onPressed: (){
+                setState(() {
+                  rightDice = Random().nextInt(6)+1;
+                });
+              }, child: const Text('Roll Only Right Dice'),
+
+                style: ButtonStyle
+                  (
+                  backgroundColor: MaterialStateProperty.all(Colors.black),
+                  shadowColor: MaterialStateProperty.all(Colors.white),
+                  foregroundColor: MaterialStateProperty.all(Colors.white),
+
+                ),
+
+
+              ),
+            )
+          ],
+        )
+      ],
     );
   }
 }
+
